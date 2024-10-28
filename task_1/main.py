9853d864af8da0cb872f3b14c8a38494b04aed90
salandr <ansaliy200013@gmail.com>
2024-10-28 22:00:15 +0000

    tasks solved

---
+++ task_1/main.py
@@ -0,0 +1,19 @@
+def caching_fibonacci() -> callable:
+    cache = {}  
+
+    def fibonacci(n: int) -> int:
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n in cache:
+            return cache[n]
+        else:
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+            return cache[n]
+
+    return fibonacci
+
+fib = caching_fibonacci()
+print(fib(20))  
+print(fib(15))  
